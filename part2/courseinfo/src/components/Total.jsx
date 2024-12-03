81e1c720f96a70704dc6086f8246f2fcbed4a222
Jahongir Hayitov <Jahongirhacking@gmail.com>
2024-12-03 12:50:24 +0500

    feat: complete part 2.3

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,8 +1,8 @@
 const Total = ({ parts }) => {
   return (
-    <p>
-      Number of exercises {parts.reduce((acc, curr) => acc + curr.exercises, 0)}
-    </p>
+    <b>
+      total of {parts.reduce((acc, curr) => acc + curr.exercises, 0)} exercises
+    </b>
   );
 };
 
