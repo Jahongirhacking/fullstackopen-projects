e5a1a89257e3e68115e1f91aeb54830e736b2a7f
Jahongir Hayitov <Jahongirhacking@gmail.com>
2024-12-18 16:24:32 +0500

    feat: complete lint part  3.22

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,94 +1,94 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const app = express();
-const PhoneBook = require('./models/phonebook');
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const PhoneBook = require('./models/phonebook')
 
-app.use(express.json());
-app.use(express.static('dist'));
+app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('req-body', (req) => {
-    return JSON.stringify(req.body);
-});
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :req-body"));
+  return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
 
 app.get('/', (req, res) => {
-    res.send('<a href="/api/persons">GET /api/persons</a>');
+  res.send('<a href="/api/persons">GET /api/persons</a>')
 })
 
 app.get('/info', (req, res, next) => {
-    PhoneBook.find({}).then(persons => {
-        res.send(`<p>
+  PhoneBook.find({}).then(persons => {
+    res.send(`<p>
         <p>Phonebook has info for ${persons.length} people</p>
         <p>${new Date()}</p>
-    </p>`);
-    }).catch(err => next(err))
+    </p>`)
+  }).catch(err => next(err))
 })
 
 app.get('/api/persons', (req, res, next) => {
-    PhoneBook.find({}).then(persons => {
-        res.json(persons);
-    }).catch(err => next(err))
+  PhoneBook.find({}).then(persons => {
+    res.json(persons)
+  }).catch(err => next(err))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id;
-    PhoneBook.findById(id).then(person => {
-        if(person) {
-            res.json(person);
-        } else {
-            res.status(404).send('No such person');
-        }
-    }).catch(error => next(error));
+  const id = req.params.id
+  PhoneBook.findById(id).then(person => {
+    if(person) {
+      res.json(person)
+    } else {
+      res.status(404).send('No such person')
+    }
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id;
-    PhoneBook.findByIdAndDelete(id).then(person => {
-        res.status(204).end();
-    }).catch(err => next(err))
+  const id = req.params.id
+  PhoneBook.findByIdAndDelete(id).then(() => {
+    res.status(204).end()
+  }).catch(err => next(err))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const newPerson = req.body;
-    if (!newPerson.name) {
-        return res.status(400).json({ error: 'name must not be empty' });
-    }
-    const person = new PhoneBook({
-        name: newPerson.name,
-        number: newPerson.number || '',
-    });
-    person.save().then(person => {
-        res.json(person);
-    }).catch(err => next(err))
-});
+  const newPerson = req.body
+  if (!newPerson.name) {
+    return res.status(400).json({ error: 'name must not be empty' })
+  }
+  const person = new PhoneBook({
+    name: newPerson.name,
+    number: newPerson.number || '',
+  })
+  person.save().then(person => {
+    res.json(person)
+  }).catch(err => next(err))
+})
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id;
-    const person = {name: req.body.name, number: req.body.number};
+  const id = req.params.id
+  const person = { name: req.body.name, number: req.body.number }
 
-    PhoneBook.findByIdAndUpdate(id, person, {new: true, runValidators: true, context: 'query'}).then(updatedPerson => {
-        res.status(200).json(updatedPerson);
-    }).catch(err => next(err))
+  PhoneBook.findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' }).then(updatedPerson => {
+    res.status(200).json(updatedPerson)
+  }).catch(err => next(err))
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({error: 'unknown endpoint'});
+  res.status(404).send({ error: 'unknown endpoint' })
 }
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (err, req, res, next) => {
-    console.error(err.message);
-    if (err.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' })
-    }
-    if(err.name === 'ValidationError') {
-        return res.status(400).send({ error: err.message });
-    }
-    next(err)
+  console.error(err.message)
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+  if(err.name === 'ValidationError') {
+    return res.status(400).send({ error: err.message })
+  }
+  next(err)
 }
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`Listening on http://localhost:${PORT}`)
 })
--- part3/phonebook/models/phonebook.js
+++ part3/phonebook/models/phonebook.js
@@ -1,39 +1,39 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
+require('dotenv').config()
+const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url).then(() => {
-    console.log('MongoDB Connected');
-}). catch(err => console.log("error connecting to MongoDB", err.message));
+  console.log('MongoDB Connected')
+}). catch(err => console.log('error connecting to MongoDB', err.message))
 
 const phonebookSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (v) => {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: 'Please enter correct number, XX-... or XXX-...',
     },
-    number: {
-        type: String,
-        validate: {
-            validator: (v) => {
-                return /^\d{2,3}-\d+$/.test(v);
-            },
-            message: 'Please enter correct number, XX-... or XXX-...',
-        },
-        minlength: 8,
-        required: [true, 'User phone number required']
-    },
+    minlength: 8,
+    required: [true, 'User phone number required']
+  },
 })
 
-phonebookSchema.set("toJSON", {
-    transform: function (doc, obj) {
-        obj.id = obj._id.toString();
-        delete obj._id;
-        delete obj.__v;
-    }
+phonebookSchema.set('toJSON', {
+  transform: function (doc, obj) {
+    obj.id = obj._id.toString()
+    delete obj._id
+    delete obj.__v
+  }
 })
 
-module.exports = mongoose.model('Phonebook', phonebookSchema);
+module.exports = mongoose.model('Phonebook', phonebookSchema)
