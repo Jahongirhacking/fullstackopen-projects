4b44fa6f0f8ce0bcd2c78c79d36efc6d6241861f
Jahongir Hayitov <Jahongirhacking@gmail.com>
2024-12-19 15:42:48 +0500

    feat: complete test part 4.4

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,7 +1,30 @@
-const test = require("node:test");
+const {test, describe} = require("node:test");
 const assert = require("node:assert");
-const helper = require('../utils/list_helper');
+const listHelper = require('../utils/list_helper');
 
 test("dummy returns one", () => {
-    assert.strictEqual(helper.dummy([]), 1);
+    assert.strictEqual(listHelper.dummy([]), 1);
+})
+
+describe('total likes', () => {
+    const blog = {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+    };
+
+    test('of empty list is zero', () => {
+        assert.strictEqual(listHelper.totalLikes([]), 0)
+    })
+
+    test('when list has only one blog, equals the likes of that', () => {
+        assert.strictEqual(listHelper.totalLikes([blog]), 5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        assert.strictEqual(listHelper.totalLikes(Array.from({length: 4}).map(() => blog)), 20)
+    })
 })
