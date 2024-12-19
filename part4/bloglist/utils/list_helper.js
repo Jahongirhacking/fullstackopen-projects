06189c87a64bee22cf4a61c396d0dd4613814141
Jahongir Hayitov <Jahongirhacking@gmail.com>
2024-12-19 15:55:24 +0500

    feat: complete test part 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
     return blogs.reduce((acc, current) => acc + current.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if(!blogs.length) return null;
+    let favorite = blogs[0];
+    for(let blog of blogs) {
+        if(blog.likes > favorite.likes) {
+            favorite = blog;
+        }
+    }
+    return favorite;
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
 }
