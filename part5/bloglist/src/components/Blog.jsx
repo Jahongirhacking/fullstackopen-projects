18baf703f80b722425ff84ea5e49ed80cfa6a988
Jahongir Hayitov <Jahongirhacking@gmail.com>
2025-01-06 20:45:10 +0500

    feat: complete part 5.11

--- part5/bloglist/src/components/Blog.jsx
+++ part5/bloglist/src/components/Blog.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { putLike } from "../services/blogs.js";
+import { putLike, removeBlog } from "../services/blogs.js";
 
 const Blog = ({ blog, getAllBlogsFromDb }) => {
   const [isCollapse, setIsCollapse] = useState(true);
@@ -9,20 +9,30 @@ const Blog = ({ blog, getAllBlogsFromDb }) => {
     await getAllBlogsFromDb();
   };
 
+  const handleRemove = async (id) => {
+    if (!window.confirm("Are you sure you want to delete this post?")) return;
+    await removeBlog(id);
+    await getAllBlogsFromDb();
+  };
+
   return (
     <div style={{ border: "2px solid black", margin: "10px 5px" }}>
-      <p>{blog.title}</p>
-      {isCollapse ? (
-        <button onClick={() => setIsCollapse(false)}>view</button>
-      ) : (
+      <p>
+        {blog.title} {blog.author}
+        <button onClick={() => setIsCollapse((prev) => !prev)}>
+          {isCollapse ? "view" : "hide"}
+        </button>
+      </p>
+
+      {!isCollapse && (
         <>
           <a href={blog.url}>{blog.url}</a>
           <p>
             likes {blog.likes}
             <button onClick={() => handleLike(blog.id)}>like</button>
           </p>
-          <p>{blog.author}</p>
-          <button onClick={() => setIsCollapse(true)}>hide</button>
+          <p>{blog?.user?.username}</p>
+          <button onClick={() => handleRemove(blog.id)}>remove</button>
         </>
       )}
     </div>
--- part5/bloglist/src/services/blogs.js
+++ part5/bloglist/src/services/blogs.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getLocalStorage, localStorageNames } from "../utils/storage.js";
 
 const baseUrl = import.meta.env.VITE_API_URL;
 
@@ -9,5 +10,14 @@ export const getAll = async () => {
 
 export const putLike = async (id) => {
   const { data } = await axios.put(`${baseUrl}/blogs/${id}/likes`);
+  return data;
+};
+
+export const removeBlog = async (id) => {
+  const { data } = await axios.delete(`${baseUrl}/blogs/${id}`, {
+    headers: {
+      Authorization: `Bearer ${getLocalStorage(localStorageNames.token)}`,
+    },
+  });
   return data;
 };
