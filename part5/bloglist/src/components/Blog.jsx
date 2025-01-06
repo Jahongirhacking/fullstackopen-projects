d662a09499aa329dd8611437111a7fd14621def2
Jahongir Hayitov <Jahongirhacking@gmail.com>
2025-01-06 20:08:08 +0500

    feat: complete part 5.7

--- part5/bloglist/src/components/Blog.jsx
+++ part5/bloglist/src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [isCollapse, setIsCollapse] = useState(true);
+  return (
+    <div style={{ border: "2px solid black", margin: "10px 5px" }}>
+      <p>{blog.title}</p>
+      {isCollapse ? (
+        <button onClick={() => setIsCollapse(false)}>view</button>
+      ) : (
+        <>
+          <p>{blog.url}</p>
+          <p>
+            likes {blog.likes}
+            <button>like</button>
+          </p>
+          <p>{blog.author}</p>
+          <button onClick={() => setIsCollapse(true)}>hide</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
